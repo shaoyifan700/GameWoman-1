a739c9038e6c81f7e4e5dc9675df43e1146c7b0f
shaoyifan700 <126126211+shaoyifan700@users.noreply.github.com>
2026-04-07 21:11:15 +0800

    XiaXinghe-DuiHua

--- Assets/Scripts/Managers/HomePageManager.cs
+++ Assets/Scripts/Managers/HomePageManager.cs
@@ -1,8 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HomePageManager : MonoBehaviour
 {
+    public AudioSource bgmAudioSource;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        // 读取上次保存的音量，默认0.5
+        float savedVolume = PlayerPrefs.GetFloat("BGMVolume", 0.5f);
+        volumeSlider.value = savedVolume;
+        bgmAudioSource.volume = savedVolume;
+
+        // 监听滑动条变化
+        volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+    }
+
+    void OnVolumeChanged(float value)
+    {
+        bgmAudioSource.volume = value;
+        // 保存音量设置
+        PlayerPrefs.SetFloat("BGMVolume", value);
+    }
     public void OnClickLinChenxi()
     {
         PlayerPrefs.SetInt("SelectedCharacter", 1);
@@ -18,7 +39,7 @@ public class HomePageManager : MonoBehaviour
     public void OnClickXiaXinghe()
     {
         PlayerPrefs.SetInt("SelectedCharacter", 3);
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("GuScene");
     }
 
     public void OnClickQuit()
